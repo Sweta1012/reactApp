ab96007703fcb72a7728e69901a8a5326efca05a
Sweta <swetap1012@gmail.com>
2019-02-04 17:01:21 -0800

    Boiler-plate
    -Structure of a React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+        fName: '',
+        Age: '',
+        Info: ''
+    }
+  }
+
+  showInfo = () => {
+    let Info = this.state.Info;
+    Info = this.Name.value + this.Age.value;
+
+    this.setState({
+      Info
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+            <label>Name: </label>
+            <input type="text" ref={(input)=>{this.Name = input}} name="name" id="name" placeholder="Name"/>
+            <br/> <br/>
+            <label>Age: </label>
+            <input type="text" ref={(input)=>{this.Age = input}} name="age" id="age" placeholder="Age"/>
+            <br/> <br/>
+            <button type="button" onClick={this.showInfo}>Submit</button>
+            <h3>{this.state.Info}</h3>
+        </form>
       </div>
     );
   }
